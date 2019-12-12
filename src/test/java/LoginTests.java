8735ffde53c8d3d0019a62bd8d3a333080c1fb56
Sofrus <sofrus@gmail.com>
2019-12-12 14:05:42 +0300

    Login tests

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -1,6 +1,7 @@
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+import io.github.cdimascio.dotenv.Dotenv;
 
 import static com.codeborne.selenide.Condition.exist;
 import static com.codeborne.selenide.Condition.visible;
@@ -9,13 +10,14 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class LoginTests {
+    Dotenv dotenv = Dotenv.load();
     @Test
     @DisplayName("Positive test")
     void positiveTest() {
         open("https://www.moscowbooks.ru/");
         $("#loginLink").click();
-        $("#Email_auth").val("sofrus@gmail.com").pressTab();
-        $("#Password").val("123456").pressEnter();
+        $("#Email_auth").setValue(dotenv.get("Login")).pressTab();
+        $("#Password").setValue(dotenv.get("Password")).pressEnter();
         $(withText("Выйти")).should(exist);
         $(withText("sofrus@gmail.com")).should(exist);
 
